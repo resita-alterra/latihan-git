4ff67bcba76d81a6de3e746b9e356ac878f95c02
resita-alterra <resita@alterra.id>
2019-07-24 14:39:28 +0700

    push mentor

--- ata/admin.py
+++ ata/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Mentor
 
+class MentorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nama', 'exp', 'quote','url_file_image']
+    ordering = ['id']
+
+admin.site.register(Mentor, MentorAdmin)
 # Register your models here.

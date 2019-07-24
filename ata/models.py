e104b5ed98ae27ceadbe0e8cafa2e3323cb7b0c6
farizap <fapriyanto@alterra.id>
2019-07-24 14:37:57 +0700

    first-commit-home

--- ata/models.py
+++ ata/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    judul = models.CharField(max_length=255)
+    gambar = models.CharField(max_length=100)
+    tanggal = models.DateField()
+    comment_cnt = models.IntegerField()
+    content = models.TextField()
+
+    def __str__(self):
+        return self.judul
